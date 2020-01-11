c2bb98c3b0ddf72e7c49aa54d6dda6ca9c325791
Alochym <you@example.com>
2020-01-11 11:37:18 +0700

    update calc todo note

---
+++ src/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"calc"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(calc.Add(2, 3))
+	fmt.Println(calc.Substract(2, 3))
+	fmt.Println(calc.Multiply(2, 3))
+	fmt.Println(calc.Divide(2, 3))
+}
