a8ee197880b64278d0f19babf68c0527019de976
Alochym <you@example.com>
2020-01-13 16:47:31 +0700

    Adding documents for all function

--- src/calc/calc.go
+++ src/calc/calc.go
@@ -1,15 +1,21 @@
 package calc
 
+// Add takes two integers and returns the sum of them.
 func Add(num1, num2 int) int {
 	return num1 + num2
 }
 
+// Substract takes two integers and returns the substract of them.
 func Substract(num1, num2 int) int {
 	return num1 - num2
 }
+
+// Multiply takes two integers and returns the multiply of them.
 func Multiply(num1, num2 int) int {
 	return num1 * num2
 }
+
+// Divide takes two integers and returns the divide of them.
 func Divide(num1, num2 int) int {
 	return num1 / num2
 }
