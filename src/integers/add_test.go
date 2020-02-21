61a8b8f3de6f59bb113006d43633b0c7c42a8b09
Alochym01 <alochym01@gmail.com>
2020-02-21 17:08:21 +0700

    add integer testing w example

---
+++ src/integers/add_test.go
@@ -0,0 +1,23 @@
+package integer
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	assertCorrectMessage := func(t *testing.T, got, want int) {
+		if got != want {
+			t.Errorf("want '%d' but got '%d'", want, got)
+		}
+	}
+	got := Add(2, 2)
+	want := 4
+	assertCorrectMessage(t, got, want)
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
---
+++ src/integers/integer.go
@@ -0,0 +1,6 @@
+package integer
+
+// Add function takes 2 integers as parametes and return an integer value
+func Add(x int, y int) int {
+	return x + y
+}
